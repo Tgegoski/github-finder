e151e6bbf50492864924d3a5c8f8789873864dc2
Tammy Gegoski <tgegoski@gmail.com>
2021-08-16 02:00:33 -0400

    added checkbox if hireable and added avatar for user

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -43,7 +43,17 @@ export class User extends Component {
                 <i className='fas fa-check text-success' />
             ) : (
                 <i className='fas fa-times-circle text-danger' />
-            )}         
+            )}  
+            <div className='card grid-2'>
+                <div className='all-center'>
+                    <img 
+                    src={avatar_url}
+                    className='round-img'
+                    alt=''
+                    style={{ width: '150px' }}
+                    />
+                </div>
+            </div>      
         </Fragment>
         ); 
     }
