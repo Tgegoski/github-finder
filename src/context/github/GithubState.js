1ceef8702973c2b18443bd9814ae8f35ed6a00fa
Tammy Gegoski <tgegoski@gmail.com>
2021-10-13 17:16:46 -0400

    set up netlify and deployed site

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,6 +10,18 @@ import {
     GET_REPOS
 } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production') {
+    githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+    githubClientId = process.env.GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+
+}
+
 const GithubState = props => {
     const initialState = {
         users: [],
@@ -25,11 +37,10 @@ const searchUsers = async text => {
     setLoading();
 
     const res = await axios.get(
-    `https://api.github.com/search/users?q=${text}&client_id=${
-    process.env.REACT_APP_GITHUB_CLIENT_ID
-    }&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
+    `https://api.github.com/search/users?q=${text}&client_id=$
+    {githubClientId}&client_secret=${githubClientSecret}`
+   
+    );
     dispatch({ 
     type: SEARCH_USERS,
     payload: res.data.items
@@ -40,25 +51,25 @@ const getUser = async (username) => {
     setLoading();
 
     const res = await axios.get(
-    `https://api.github.com/users/${username}?client_id=${
-    process.env.REACT_APP_GITHUB_CLIENT_ID
-    }&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
+    `https://api.github.com/users/${username}?client_id=$
+    {githubClientId}&client_secret=$
+    {githubClientSecret}`
+    );
    dispatch({ 
        type: GET_USER,
        payload: res.data
    });
-  };
+};
 // Get Repos
 const getUserRepos = async username => {
     setLoading();
 
   const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
-      process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=$
-      {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
+      {githubClientId}
+      &client_secret=$
+      {githubClientSecret}`
+      );
   
     dispatch({ 
         type: GET_REPOS,
@@ -70,6 +81,7 @@ const clearUsers = () => dispatch({ type: CLEAR_USERS });
    
 // Set Loading
 const setLoading = () => dispatch({ type: SET_LOADING });
+
 return (
     <GithubContext.Provider
     value={{ 
